27b9ffcc0b65258bf1b4c3a3fae64a06ea36713f
mitakawe123 <dkyuchukov2003@gmail.com>
2022-07-24 11:18:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const data = [
+    { id: 12, name: "georgi" },
+    { id: 3, name: "ivana" },
+    { id: 33, name: "bishop" },
+    { id: 41, name: "gabriel" },
+  ];
+
+  const exportData = (tableID, filename = "") => {
+    const dataType = "application/vnd.ms-excel";
+    const tableSelect = document.getElementById("tableID");
+    const tableHTML = tableSelect.outerHTML.replace(/ /g, "%20");
+
+    filename = filename ? filename + ".xls" : "Data.xls";
+    let downloadLink = document.createElement("a");
+    document.body.appendChild(downloadLink);
+
+    if (navigator.msSaveOrOpenBlob) {
+      const blob = new Blob(["\ufeff", tableHTML], {
+        type: dataType,
+      });
+      navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+      downloadLink.href = "data:" + dataType + ", " + tableHTML;
+
+      downloadLink.download = filename;
+      downloadLink.click();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table id="tableID">
+        {data.map((sheet) => (
+          <div key={sheet.id}>
+            <tr>
+              <th>{sheet.id}</th>
+              <th>{sheet.name}</th>
+            </tr>
+          </div>
+        ))}
+      </table>
+      <button onClick={exportData}>Export</button>
     </div>
   );
 }
